650c6684ee5d3482f46806c2ecb9203f4da9d8b0
meg-github <fishfishfish@gmail.com>
2022-09-23 17:24:29 +0900

    fix

--- ant/pages/＊質問回答.py
+++ ant/pages/＊質問回答.py
@@ -9,5 +9,5 @@ from PIL import Image
 
 
 st.components.v1.html(
-'<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd5aP0zgMk59NdhzfaUohiuzCZ4NWOrUNgAzGDL488srdwStg/viewform?embedded=true" width="640" height="1431" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>'
+'<iframe width="95%" src="https://docs.google.com/forms/d/e/1FAIpQLSd5aP0zgMk59NdhzfaUohiuzCZ4NWOrUNgAzGDL488srdwStg/viewform?embedded=true" width="640" height="1431" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>'
 ,height = 2000)
